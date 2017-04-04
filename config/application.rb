3743343b6e9bc708345dd4ac623ec5e06374190a
Laura Kelly <laura.kelly@gmail.com>
2017-04-04 21:40:28 +0000

    trying to figure out font issue

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,28 @@
-require_relative 'boot'
+#require_relative 'boot'
+
+#require 'rails/all'
+
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+
+Bundler.require(:default, Rails.env)
 
 module Dejabbq
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    
   end
 end
