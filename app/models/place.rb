1dadd8dbd9333ccae92ad2a85ea847b5b99746e2
Laura Kelly <laura.kelly@gmail.com>
2017-04-07 16:31:38 +0000

    pagination sorted out

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,9 @@
 class Place < ApplicationRecord
+    def self.search(term, page)
+      if term
+        where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 5).order('id DESC')
+      else
+        paginate(page: page, per_page: 3).order('id DESC') 
+      end
+    end
 end
