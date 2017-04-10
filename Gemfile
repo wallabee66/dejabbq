9e28900c2742e76f98571f7d727709327fe4d8c6
Laura Kelly <laura.kelly@gmail.com>
2017-04-10 17:50:42 +0000

    add form

--- Gemfile
+++ Gemfile
@@ -54,4 +54,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
 
+gem 'simple_form'
+
 gem 'will_paginate', '>= 3.1'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,9 @@ class PlacesController < ApplicationController
     def index
         @places = Place.paginate(:page => params[:page], :per_page => 3)
     end
+    
+    def new
+        @place = Place.new
+    end
+    
 end
