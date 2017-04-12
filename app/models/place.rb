d91147cf66d6c1aa39c49641fd4dae03d5186673
Laura Kelly <laura.kelly@gmail.com>
2017-04-12 19:29:37 +0000

    connecting places, lesson 18

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
+#class Place < ApplicationRecord
+#    def self.search(term, page)
+#      if term
+#        where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 5).order('id DESC')
+#      else
+#        paginate(page: page, per_page: 3).order('id DESC') 
+#      end
+#    end
+#end
 class Place < ApplicationRecord
-    def self.search(term, page)
-      if term
-        where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 5).order('id DESC')
-      else
-        paginate(page: page, per_page: 3).order('id DESC') 
-      end
-    end
+  belongs_to :user
 end
