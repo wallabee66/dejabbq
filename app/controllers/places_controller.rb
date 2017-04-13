b44082f142ad157e30f2396b3734fb86fef42e9a
Laura Kelly <laura.kelly@gmail.com>
2017-04-13 18:43:10 +0000

    lesson 21, place detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
+    
     def index
         @places = Place.paginate(:page => params[:page], :per_page => 3)
     end
@@ -11,6 +12,10 @@ class PlacesController < ApplicationController
     def create
         current_user.places.create(place_params)
         redirect_to root_path
+    end
+    
+    def show
+        @place = Place.find(params[:id])
     end
     
     private
