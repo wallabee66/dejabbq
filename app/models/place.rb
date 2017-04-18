cda0d7fbde0e462daf71f516c9b25301cb6202af
Laura Kelly <laura.kelly@gmail.com>
2017-04-18 15:01:42 +0000

    lesson 28 - validations challenge

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,5 +9,7 @@
 #end
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
