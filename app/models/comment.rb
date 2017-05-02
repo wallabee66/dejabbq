02d9d2c379ef8af4fc10ea14f2fa149bbe5c3695
Laura Kelly <laura.kelly@gmail.com>
2017-05-02 19:48:25 +0000

    lesson 43

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    after_create :send_comment_email
+    
     
     RATINGS = {
         'one star': '1_star',
@@ -12,5 +14,9 @@ class Comment < ApplicationRecord
     
     def humanized_rating
         RATINGS.invert[self.rating]
+    end
+    
+    def send_comment_email
+        NotificationMailer.comment_added(self).deliver
     end
 end
