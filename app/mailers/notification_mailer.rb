2110af406cff33fc60746bec9a856aa398a7b469
Laura Kelly <laura.kelly@gmail.com>
2017-05-02 18:40:13 +0000

    lesson 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@dejabbq.com"
     
     def comment_added
-        mail(to: "laura@dejabbq.com", subject: "A comment has been added to your place")
+        mail(to: "dejabbqtest@gmail.com", subject: "A comment has been added to your place")
     end
 end
